574c389a561c859d89c205e317ed9b4eabd30d7e
vduartev <duarte1177@gmail.com>
2013-10-10 20:24:19 -0300

    ListaDeGrupos

---
+++ src/com/example/interfacesp/ListaGrupos.java
@@ -0,0 +1,13 @@
+package com.example.interfacesp;
+
+import android.app.Activity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by victor on 10-10-13.
+ */
+public class ListaGrupos extends Activity {
+    ArrayList <String> listaGrupo = new ArrayList<String>(); // Puedo obtener datos desde el telefono.
+
+}
