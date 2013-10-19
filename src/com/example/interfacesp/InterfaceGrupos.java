abf5f0a80c2e069af50c9efc35c2ec6e576bdb1e
Alejandro Silva <pm5k.sk@gmail.com>
2013-10-18 22:18:22 -0300

    Servicio corriendo
    - el servicio se levanta al detectar una coneccion a internet
    - ahora funciona el boton de menu de la aplicacion

--- src/com/example/interfacesp/InterfaceGrupos.java
+++ src/com/example/interfacesp/InterfaceGrupos.java
@@ -1,20 +1,18 @@
 package com.example.interfacesp;
-
 import java.util.ArrayList;
-
-import android.app.Activity;
+import servicios.ServiceConfig;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +22,7 @@ public class InterfaceGrupos extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Apéndice de método generado automáticamente
 		super.onCreate(savedInstanceState);
+		// asociar el codigo a la vista interfacegrupo1
 		setContentView(R.layout.interfacegrupo1);
 		
 		
@@ -35,23 +34,19 @@ public class InterfaceGrupos extends ListActivity {
 		
 		IconListViewAdapter ilva = new IconListViewAdapter(this, R.layout.interfacegrupo, locales );
 		setListAdapter(ilva);
-		
-		
 	}
 
 	public void InterfaceMensajes(View view){
-		
+		// mostrar la actividad InterfaceMensajes
 		Intent a = new Intent (this, InterfaceMensajes.class);
 		startActivity(a);
 	}
 
     public class IconListViewAdapter extends ArrayAdapter<String> {
-
         private ArrayList<String> items;
         
         protected void onCreate(Bundle savedInstanceState) {
-    	
-        	
+        	// falta codigo??
         }
         
         public IconListViewAdapter(Context context, int textViewResourceId, ArrayList<String> items) {
@@ -74,7 +69,6 @@ public class InterfaceGrupos extends ListActivity {
                         TextView tt = (TextView) v.findViewById(R.id.tipogrupo);
                         tt.setText(o);
                 
-                
 		                CheckBox checkBox1 = (CheckBox) v.findViewById(R.id.checkBox1);
 		       		 
 		        		checkBox1.setOnClickListener(new OnClickListener() {
@@ -84,13 +78,12 @@ public class InterfaceGrupos extends ListActivity {
 		        	                //is chkIos checked?
 		        			if (((CheckBox) v).isChecked()) {
 		        				Toast.makeText(InterfaceGrupos.this,
-		        			 	   "Entrando en "+o, Toast.LENGTH_LONG).show();
+		        			 	   "Entrando en "+o, Toast.LENGTH_SHORT).show();
 		        			}
 		        			
 		        			else {
 		        				Toast.makeText(InterfaceGrupos.this,
-		        					 	   "Saliendo de "+o, Toast.LENGTH_LONG).show();
-		        						
+		        					 	   "Saliendo de "+o, Toast.LENGTH_SHORT).show();
 		        			}
 		        		  }
 		        		});
@@ -98,5 +91,22 @@ public class InterfaceGrupos extends ListActivity {
                 
                 return v;
         }
-}
+    }
+    
+	// MENU INFERIOR
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.aplicacion_settings, menu);
+		return true;
+	}
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.settings_application:
+        		Intent i = new Intent(this, ServiceConfig.class);
+        		startActivity(i);
+               break;
+        }
+        // true -> consumimos el item, detener la propagacion
+        return true; 
+    }
 }
---
+++ src/servicios/ServicioMensajes.java
@@ -0,0 +1,59 @@
+package servicios;
+
+import java.util.Calendar;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class ServicioMensajes extends Service {
+	       //MediaPlayer reproductor;
+	 
+	       @Override
+	       public void onCreate() {
+	             Toast.makeText(this,"Servicio creado", Toast.LENGTH_SHORT).show();
+	             Log.i("SERVICE", "servicio creado");
+	       }
+	       
+	       /*
+	       @Override
+	       public void onStart(Intent intent, int idArranque) {
+	              Toast.makeText(this, "Servicio arrancado " + idArranque,Toast.LENGTH_SHORT).show();
+	              Log.i("SERVICE", "servicio arrancado "+idArranque);
+	              //reproductor.start();
+	       }
+	        */
+	       /**
+	       		intent		intent usado en startService( intent );
+	       		flags		Información sobre como comienza la solicitud. Puede ser: 0, START_FLAG_REDELIVERY, START_FLAG_RETRY. 
+	       					Un valor distinto de 0 se utiliza para reiniciar un servicio tras detectar algún problema.
+	       		idArranque 	representa el numero de solicitud de arranque. Usar este mismo estero en el método stopSelfResult(int idArranque).
+	       		return		Como debe comportarse el sistema cuando el servicio sea matado. (ej. pore el sistema por falta de memoria)
+	       */
+	       @Override
+	       public int onStartCommand(Intent intent, int flags, int idArranque) {
+	             Toast.makeText(this,"Servicio "+idArranque+" arrancado ", Toast.LENGTH_SHORT).show();
+	             Log.i("SERVICE", "Servicio "+idArranque+" arrancado ");
+	             	             
+	             // revisar si hay mensajes nuevos
+	             stopSelf();
+	             return START_NOT_STICKY;
+	       }
+
+	 
+	       @Override
+	       public void onDestroy() {
+	             Toast.makeText(this,"Servicio detenido", Toast.LENGTH_SHORT).show();
+	             Log.i("SERVICE", "servicio detenido");
+	       }
+	 
+	       @Override
+	       public IBinder onBind(Intent intencion) {
+	             return null;
+	       }
+	}
