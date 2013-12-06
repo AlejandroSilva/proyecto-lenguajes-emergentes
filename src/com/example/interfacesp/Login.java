7248b670b97853498b13d8f2945b0f2e82c40cf9
Pamela Jaramillo <pam2511@gmail.com>
2013-12-06 13:28:53 -0200

    Update Login.java

--- src/com/example/interfacesp/Login.java
+++ src/com/example/interfacesp/Login.java
@@ -1,25 +1,25 @@
 package com.example.interfacesp;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 
 public class Login extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.principal);
-	}
-	public void InterfaceGrupos(View view){
-		
-		Intent registro = new Intent (this, InterfaceGrupos.class);
-		startActivity(registro);
-		
-	}
-
-	
-
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                // asociar con la vista principal (login)
+                setContentView(R.layout.principal);
+        }
+        
+        // al hacer click en el boton de login
+        public void InterfaceGrupos(View view){
+                Intent registro = new Intent (this, InterfaceGrupos.class);
+                startActivity(registro);
+                
+                // Configuracion de servicio
+                //Intent serviceConfig = new Intent( this, ServiceConfig.class);
+                //startActivity( serviceConfig );
+        }
 }
